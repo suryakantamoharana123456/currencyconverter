6eb2d30f3a9e7066b60186bef7ec50077f82724c
suryakantamoharana123456 <suryakantamoharana60019@gmail.com>
2024-07-29 18:32:02 +0000

    jhjg

--- currency/src/Hooks/useCurrencyInfo.js
+++ currency/src/Hooks/useCurrencyInfo.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency){
     const [data, setData] = useState({});
     useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://openexchangerates.org/api/currencies/${currency}.json`)
         .then((response) => response.json())
         .then((response) => setData(response[currency]))
         console.log(data)
